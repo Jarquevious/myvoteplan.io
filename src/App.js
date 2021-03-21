c8606a2a235a87ad1b5bb583856210578f91dcdd
Jarquevious <jarquevious@gmail.com>
2021-03-21 15:11:15 -0700

    FormUserDetails Not Working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
-import Navbar from './components/navbar/navbar'
-import Jumbotron from './components/jumbotron/jumbrotron'
-import ReactDOM from 'react-dom';
-import HorizontalLabelPositionBelowStepper from './components/progress-bar/progress-bar';
-import ContainedButtons from './components/create_plan_button/create_plan_button';
-import SignupForm from './components/aboutUser/aboutUser'
+import UserForm from './components/UserForm';
 
-ReactDOM.render(<HorizontalLabelPositionBelowStepper />, document.querySelector('#root'));
+
+// ReactDOM.render(<HorizontalLabelPositionBelowStepper />, document.querySelector('#root'));
 
 
  
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Jumbotron/>
-      <HorizontalLabelPositionBelowStepper/>
-      <ContainedButtons />
-      <SignupForm />
-      
+    <div className="body">
+      <UserForm />
     </div>
   );
 }
